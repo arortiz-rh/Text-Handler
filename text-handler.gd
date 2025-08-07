95f166c2f3eb88e2ece13b399d297a3fe80c9b47
Armando Ortiz <arortiz@redhat.com>
2025-08-07 11:28:45 -0400

    Add: Functionality for text display options
    - All text movement options now function as expected.
    - Added default values to the speeds.

--- text-handler.gd
+++ text-handler.gd
@@ -7,7 +7,7 @@ enum Text_Movement_Options {
 	## Displays the text one character at a time.
 	By_Character,
 	## Displays the text instantly.
-	All_at_Once
+	Instant
 }
 
 
@@ -15,15 +15,15 @@ enum Text_Movement_Options {
 @export var json_file : JSON = JSON.new()
 
 ## Choose how you want the text to be displayed.
-@export var text_movement : Text_Movement_Options
+@export var text_movement : Text_Movement_Options = Text_Movement_Options.Instant
 
 ## Sets how many characters are displayed per minute, only applicable when Text Movement is set to
 ## "By Character". All negative values will be treated as 0.
-@export var char_per_minute : int
+@export var char_per_minute : int = 100
 
 ## Sets how many words are displayed per minute, only applicable when Text Movement is set to
 ## "By Word". All negative values will be treated as 0.
-@export var word_per_minute : int
+@export var word_per_minute : int = 62
 
 ## Emitted after displaying final line of the inputted json.
 signal end_of_dialogue
@@ -31,30 +31,59 @@ signal end_of_dialogue
 ## Emitted when the current line of text has finished displaying.
 signal end_of_line
 
+
 var text_holder : Array
 var current_speaker : String = ""
 var current_line : String = ""
+var dialogue_reached_end : bool = false
+var line_reached_end : bool = false
 
 func _ready() -> void:
-	process_json()
+	begin_dialogue()
 
 func begin_dialogue() -> void:
-	pass
+	_process_json()
+	next_line()
 
 func next_line() -> void:
-	pass
+	var line_and_name : Dictionary = text_holder.pop_front()
+	current_speaker = line_and_name.get("name")
+	current_line = line_and_name.get("line")
+
+	if text_holder.size() == 0:
+		dialogue_reached_end = true
+	else:
+		dialogue_reached_end = false
+
+	self.clear()
+
+	match text_movement:
+		Text_Movement_Options.By_Word:
+			_word_by_word(current_line)
+		Text_Movement_Options.By_Character:
+			_character_by_character(current_line)
+		Text_Movement_Options.Instant:
+			_instant(current_line)
 
 # HELPER FUNCTIONS
 func _word_by_word(line : String) -> void:
-	pass
+	var word_array : PackedStringArray = line.split(" ", true)
+	
+	for i in word_array.size():
+		self.append_text(word_array[i])
+		if i != word_array.size():
+			self.append_text(" ")
+		await get_tree().create_timer(60 / word_per_minute).timeout
 
 func _character_by_character(line : String) -> void:
-	pass
+	for i in line.length():
+		self.append_text(line[i])
+		await get_tree().create_timer(60 / char_per_minute).timeout
 
-func _all_at_once(line : String) -> void:
-	pass
+func _instant(line : String) -> void:
+	self.append_text(line)
 
-func process_json() -> void:	
+func _process_json() -> void:	
 	if json_file.get_data() == null:
 		assert(false, "No JSON inputted OR no data within JSON")
 	
@@ -79,3 +108,9 @@ func get_word_speed() -> int:
 
 func get_json() -> JSON:
 	return json_file
+
+func get_end_of_dialogue() -> bool:
+	return dialogue_reached_end
+
+func get_end_of_line() -> bool:
+	return line_reached_end
